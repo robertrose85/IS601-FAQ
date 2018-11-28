806961c8b668fe76413a2d3e44b5d5ae799e339c
Bob Rose <rr637@njit.edu>
2018-11-27 21:30:15 -0500

    Fixed: Profile page and presentation

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,12 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/user/{user_id}/profile', 'ProfileController@create')->name('profile.create');
 Route::get('/user/{user_id}/profile/{profile_id}', 'ProfileController@show')->name('profile.show');
 
+Route::get('/questions/{question_id}/answer/{answer_id}', 'AnswerController@show')->name('answer.show');
+
+Route::resources([
+    'question' => 'QuestionController',
+]);
