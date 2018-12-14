f570a6808d72f9f6518129bdd9bb4d090275e7da
Bob Rose <rr637@njit.edu>
2018-12-13 22:54:29 -0500

    Added: Google Analytics Tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,16 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-131024512-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-131024512-1');
+    </script>
 </head>
 <body>
 <div id="app">
